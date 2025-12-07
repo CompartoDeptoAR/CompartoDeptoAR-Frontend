4b63dbb339edc6a2ddb4bd7fc11b51eb3602a3d2
cramer666 <luciaperrone.unlp@gmail.com>
2025-12-07 19:53:42 -0300

    Un poco de css y logica

--- src/paginas/Nosotros/ContactanosPage.tsx
+++ src/paginas/Nosotros/ContactanosPage.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import { useContactanos } from "../../hooks/pagina/nosotros/useContactanos";
 import FormularioGenerico from "../../componentes/Nosotros/FormContactanosGenerico";
 
-
 const ContactanosPage: React.FC = () => {
   const contactanosProps = useContactanos();
 
   return (
-    <FormularioGenerico
-      {...contactanosProps}
-      titulo="Contáctanos"
-      mensajePlaceholder="Escribí tu mensaje"
-      maxPalabras={300}
-    />
+    <div className="py-4">
+      <FormularioGenerico
+        titulo="Contactanos"
+        {...contactanosProps}
+      />
+    </div>
   );
 };
 
