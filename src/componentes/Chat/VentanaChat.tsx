40d6d8c06a79f999cfb74dafa16b99f85353db63
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-07 01:59:01 +0000

    render fix

--- src/componentes/Chat/VentanaChat.tsx
+++ src/componentes/Chat/VentanaChat.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { Conversacion, MensajeUI } from "../../../services/chat/ types";
+import { Conversacion, MensajeUI } from "../../services/chat/ types";
 
 interface VentanaChatProps {
   mensajes: MensajeUI[];
