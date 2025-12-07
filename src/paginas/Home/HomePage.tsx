eb3273c02f61ab6732a99a42b3cbb3d640e06f0f
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-07 08:44:45 +0000

    Cambos para ver

--- src/paginas/Home/HomePage.tsx
+++ src/paginas/Home/HomePage.tsx
@@ -1,10 +1,11 @@
 import TodasLasPublicaciones from "../Publicacion/Listar/TodasLasPublicaciones";
 import { Navegar } from "../../navigation/navigationService";
+import "../../styles/HomePage.css";
 
 const HomePage = () => {
     return (
         <div className="text-center">
-            <h1 className="mb-4">Bienvenido a CompartoDeptoAR</h1>
+            <h1 className="titulo-home mb-4">Bienvenido a CompartoDeptoAR</h1>
             <button className="btn btn-primary" onClick={() => Navegar.crearPublicacion()}>
                 Nueva Publicación
             </button>
